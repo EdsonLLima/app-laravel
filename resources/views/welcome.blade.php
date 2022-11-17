7fdcb81aaa9e2be64fd86319822cc5e9ca598354
edsonllima <focusti@hotmail.com>
2022-11-17 17:18:59 -0300

    add logo laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,13 +12,17 @@
     <!-- CSS Bootstrap -->    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
+    <!--CSS do projeto -->
+    <link rel="stylesheet" href="/css/style.css">
+
     </head>
     <body>
         <header>
             Menu
         </header>
-        <main>
+        <main class="main text-center">
             <h1>Página Inicial do Projeto</h1>
+            <img src="/img/laravel_logo.png" alt="Laravel Logo">
         </main>
         <footer>
             <p>App-Laravel &copy;2022</p>
